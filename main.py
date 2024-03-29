554cedeef170973602f0f857bc8060833f39f65f
mikic202 <mikolaj.chomanski@gmail.com>
2024-03-29 11:18:24 +0100

    add requirenments and simple code

---
+++ main.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+import gymnasium as gym
+
+print(gym.__version__)
+
+env = gym.make("MountainCar-v0", render_mode="human")
+observation, info = env.reset(seed=123, options={})
+
+done = False
+while not done:
+    action = (
+        env.action_space.sample()
+    )  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    done = terminated or truncated
+
+env.close()
